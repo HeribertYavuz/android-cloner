caedd4aeadf7a3f9c9df50a4870a9b50e3d1bbfa
Yusuf Çağrı Yavuz <info@yusufcagriyavuz.com>
2025-08-07 18:48:28 +0200

    Update main.sh
    added rsync

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install bc bison build-essential zstd clang flex gnupg gperf ccache libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git gcc g++ libssl-dev openssl -y gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf lld gcc-arm-linux-gnueabi llvm
+sudo apt install bc bison build-essential zstd clang flex gnupg gperf ccache libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git gcc g++ libssl-dev openssl -y gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf lld gcc-arm-linux-gnueabi llvm rsync
 mkdir -p ~/bin
 echo $PATH
 export PATH=~/bin:$PATH
